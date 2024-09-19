631dce29df0d88439df98ffbac46b8245376b216
Luca-Calamo <lucacalamo13@gmail.com>
2024-09-19 15:42:46 -0800

    initalize stables and add a horse

---
+++ stables.js
@@ -0,0 +1,11 @@
+console.log("horses are great!")
+
+let horseName = "Coolguy" // this is a string, perfer-always use double quotes
+horseName = "Supreme Leader" ;
+let horseAge = 2000;
+console.log(horseName) ;
+console.log(horseAge);
+let isHorseInStable = true; 
+console.log(isHorseInStable);
+isHorseInStable = false;
+console.log
