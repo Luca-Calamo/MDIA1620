3a27e1d02f9722a70abe2aa6e0764f68c74c3039
Luca-Calamo <lucacalamo13@gmail.com>
2024-09-26 15:51:04 -0800

    Lesson 4 lab

--- stables.js
+++ stables.js
@@ -14,7 +14,7 @@ const STABLE_MONTHLY_FEE = 500;
 let horseAgeIn10Years = horseAge + 10;
 let horseAgeIn20Years = horseAgeIn10Years + 10;
 console.log(STABLE_MONTHLY_FEE);
-console.log(STABLE_MONTHLY_FEE + STABLE_MONTHLY_FEE - 200);
+
 console.log(STABLE_MONTHLY_FEE * 24 / 2)
 console.log(horseAgeIn10Years);
 console.log(horseAgeIn20Years)
@@ -23,6 +23,20 @@ let horseIntroduction = " is the name of my horse"
 console.log(horseName + horseIntroduction);
 let rentIntroduction = "It costs " + STABLE_MONTHLY_FEE + " to board " + horseName;
 console.log(rentIntroduction)
-//let stableIntroduction = "Welcome to " + horseName + "'s stable";
+
 let stableIntroduction = `Welcome to "${horseName}'s stable`;
 console.log(stableIntroduction)
+
+
+///// lesson 4 code //////
+let horseNickname = "Mr President";
+let horseIntro2 = " is a very famous horse"
+console.log(horseNickname + horseIntro2)
+
+let horseMessage = "A 3 month stable fee with a 10% discount would come out to " + "$" + (STABLE_MONTHLY_FEE*3 -150) 
+console.log(horseMessage)
+
+
+
+
+
