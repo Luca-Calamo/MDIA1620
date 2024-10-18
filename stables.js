9efcdb5ae72117ec78dfe0519446f99a6d55b4bd
Luca-Calamo <lucacalamo13@gmail.com>
2024-10-18 12:27:25 -0800

    lesson7

--- stables.js
+++ stables.js
@@ -1,4 +1,3 @@
-console.log("horses are great!")
 
 let horseName = "Coolguy" // this is a string, perfer-always use double quotes
 horseName = "Supreme Leader" ;
@@ -7,9 +6,11 @@ console.log(horseName) ;
 console.log(horseAge);
 let isHorseInStable = true; 
 console.log(isHorseInStable);
- isHorseInStable = false;
+ isHorseInStable = false, true;
 let horsenickname = "Mr. President";
 console.log(horsenickname);;
+const STABLES_MONTHLY_FEE = 500;
+console.log(STABLES_MONTHLY_FEE)
 
 // LESSON 6 CODE
 //let word = "Rainbow";
@@ -20,12 +21,13 @@ let horseTwo = ["Suprme Leader", 2000, false, "Mr. President"];
 let areHorsesInside = [isHorseInStable, false, true];
 let horseNames = [horseName, "Doran", "Mikeypoo"];
 let horseStatus = `All ${horseNames.length} horses are currently outside frolicking at a Diddy party🎉🎉`;
-console.log(horseStatus);
+let horseAges = [2000, 13, 75]
+let horseNicknames = [horseName, "Luden", "Captian EA"]
 
-console.log(horseStatus);
+console.log(horseNicknames)
+console.log(horseAges);
 console.log(horseTwo);
 console.log(horseOne[0] + "'s nickname is " + horseTwo[3]);
-console.log(horseTwo)
 console.log(horseOne.length)
 horseOne.push("Purple");
 console.log(horseOne);
@@ -46,4 +48,29 @@ if (isHorseInStable) {
    console.log(horseStatus);
 }
 
+//LESSON 7 CODE
 
+function loghorseAges(horse) {
+   console.log(horseNames[horse] + " is " + horseAges[horse] + "!");
+}
+ loghorseAges(1)
+
+ function loghorseNicknames(horse, friend) {
+   const string = `${horseNames[horse]}'s nickname is ${horseNicknames[horse]} and his friend ${horseNames[friend]}'s nickname is ${horseNicknames[friend]}`;
+   console.log(string);
+ }
+
+ loghorseNicknames(2, 1);
+
+let templateLiteralFriends = `${horseNames[0]} and ${horseNames[1]} are the best of friends!`;
+console.log(templateLiteralFriends)
+
+function payMe(rent, discount) {
+   if (discount) {
+      return rent - discount;
+   } else {
+      return rent;
+   }
+}
+
+console.log(payMe(500, 100))
